5c6771618d4dc7e43ff632f677962c3e919f0f99
kurisu21 <47538257+kurisu21@users.noreply.github.com>
2020-10-17 13:02:56 -0700

    Update RAM.vhd

--- Working With VHDL/RAM.vhd
+++ Working With VHDL/RAM.vhd
@@ -4,7 +4,12 @@ use IEEE.NUMERIC.STD.ALL;
 
 entity RAM is
   port(
-    );
+    Clk   :in STD_LOGIC;
+    Rst   :in STD_LOGIC;
+    EN    :in STD_LOGIC;
+    x     :in STD_LOGIC_VECTOR(15 downto 0);
+    y     :out STD_LOGIC_VECTOR(15 downto 0);
+  );
  end entity;
     
  architecture behavioral of RAM is
